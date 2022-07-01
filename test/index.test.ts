63dda7ec4d1e5b0f78078e1800f385856bd73e98
azu <azuciao@gmail.com>
2022-07-01 16:06:10 +0900

    test: update snapshot

--- test/index.test.ts
+++ test/index.test.ts
@@ -55,28 +55,40 @@ describe("app snapshot", function () {
                         path: "/getGameById",
                         middlewares: ["requireRead"],
                         range: [288, 337],
-                        loc: { start: { line: 11, column: 0 }, end: { line: 11, column: 49 } }
+                        loc: {
+                            start: { line: 11, column: 0, index: 288 },
+                            end: { line: 11, column: 49, index: 337 }
+                        }
                     },
                     {
                         method: "get",
                         path: "/getGameList",
                         middlewares: ["requireRead"],
                         range: [339, 388],
-                        loc: { start: { line: 12, column: 0 }, end: { line: 12, column: 49 } }
+                        loc: {
+                            start: { line: 12, column: 0, index: 339 },
+                            end: { line: 12, column: 49, index: 388 }
+                        }
                     },
                     {
                         method: "post",
                         path: "/updateGameById",
                         middlewares: ["requireWrite"],
                         range: [390, 444],
-                        loc: { start: { line: 13, column: 0 }, end: { line: 13, column: 54 } }
+                        loc: {
+                            start: { line: 13, column: 0, index: 390 },
+                            end: { line: 13, column: 54, index: 444 }
+                        }
                     },
                     {
                         method: "delete",
                         path: "/deleteGameById",
                         middlewares: ["requireWrite"],
                         range: [446, 502],
-                        loc: { start: { line: 14, column: 0 }, end: { line: 14, column: 56 } }
+                        loc: {
+                            start: { line: 14, column: 0, index: 446 },
+                            end: { line: 14, column: 56, index: 502 }
+                        }
                     }
                 ]
             },
@@ -88,14 +100,20 @@ describe("app snapshot", function () {
                         path: "/user",
                         middlewares: ["user"],
                         range: [140, 162],
-                        loc: { start: { line: 8, column: 0 }, end: { line: 8, column: 22 } }
+                        loc: {
+                            start: { line: 8, column: 0, index: 140 },
+                            end: { line: 8, column: 22, index: 162 }
+                        }
                     },
                     {
                         method: "use",
                         path: "/game",
                         middlewares: ["game"],
                         range: [164, 186],
-                        loc: { start: { line: 9, column: 0 }, end: { line: 9, column: 22 } }
+                        loc: {
+                            start: { line: 9, column: 0, index: 164 },
+                            end: { line: 9, column: 22, index: 186 }
+                        }
                     }
                 ]
             },
@@ -107,28 +125,40 @@ describe("app snapshot", function () {
                         path: "/getUserById",
                         middlewares: ["requireRead"],
                         range: [287, 336],
-                        loc: { start: { line: 10, column: 0 }, end: { line: 10, column: 49 } }
+                        loc: {
+                            start: { line: 10, column: 0, index: 287 },
+                            end: { line: 10, column: 49, index: 336 }
+                        }
                     },
                     {
                         method: "get",
                         path: "/getUserList",
                         middlewares: ["requireRead"],
                         range: [338, 387],
-                        loc: { start: { line: 11, column: 0 }, end: { line: 11, column: 49 } }
+                        loc: {
+                            start: { line: 11, column: 0, index: 338 },
+                            end: { line: 11, column: 49, index: 387 }
+                        }
                     },
                     {
                         method: "post",
                         path: "/updateUserById",
                         middlewares: ["requireWrite"],
                         range: [389, 443],
-                        loc: { start: { line: 12, column: 0 }, end: { line: 12, column: 54 } }
+                        loc: {
+                            start: { line: 12, column: 0, index: 389 },
+                            end: { line: 12, column: 54, index: 443 }
+                        }
                     },
                     {
                         method: "delete",
                         path: "/deleteUserById",
                         middlewares: ["requireWrite"],
                         range: [445, 501],
-                        loc: { start: { line: 13, column: 0 }, end: { line: 13, column: 56 } }
+                        loc: {
+                            start: { line: 13, column: 0, index: 445 },
+                            end: { line: 13, column: 56, index: 501 }
+                        }
                     }
                 ]
             }
@@ -150,28 +180,40 @@ describe("app snapshot", function () {
                         path: "/getGameById",
                         middlewares: ["requireRead"],
                         range: [288, 337],
-                        loc: { start: { line: 11, column: 0 }, end: { line: 11, column: 49 } }
+                        loc: {
+                            start: { line: 11, column: 0, index: 288 },
+                            end: { line: 11, column: 49, index: 337 }
+                        }
                     },
                     {
                         method: "get",
                         path: "/getGameList",
                         middlewares: ["requireRead"],
                         range: [339, 388],
-                        loc: { start: { line: 12, column: 0 }, end: { line: 12, column: 49 } }
+                        loc: {
+                            start: { line: 12, column: 0, index: 339 },
+                            end: { line: 12, column: 49, index: 388 }
+                        }
                     },
                     {
                         method: "post",
                         path: "/updateGameById",
                         middlewares: ["requireWrite"],
                         range: [390, 444],
-                        loc: { start: { line: 13, column: 0 }, end: { line: 13, column: 54 } }
+                        loc: {
+                            start: { line: 13, column: 0, index: 390 },
+                            end: { line: 13, column: 54, index: 444 }
+                        }
                     },
                     {
                         method: "delete",
                         path: "/deleteGameById",
                         middlewares: ["requireWrite"],
                         range: [446, 502],
-                        loc: { start: { line: 14, column: 0 }, end: { line: 14, column: 56 } }
+                        loc: {
+                            start: { line: 14, column: 0, index: 446 },
+                            end: { line: 14, column: 56, index: 502 }
+                        }
                     }
                 ]
             },
@@ -183,7 +225,10 @@ describe("app snapshot", function () {
                         path: "/game",
                         middlewares: ["game"],
                         range: [167, 189],
-                        loc: { start: { line: 7, column: 0 }, end: { line: 7, column: 22 } }
+                        loc: {
+                            start: { line: 7, column: 0, index: 167 },
+                            end: { line: 7, column: 22, index: 189 }
+                        }
                     }
                 ]
             }
