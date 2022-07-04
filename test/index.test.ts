f2c4a1b40c1e6d2f048a4cbcc1a86158a54a740e
azu <azuciao@gmail.com>
2022-07-04 09:27:11 +0900

    fix

--- test/index.test.ts
+++ test/index.test.ts
@@ -30,17 +30,17 @@ describe("app snapshot", function () {
         assert.strictEqual(
             mdResults,
             `\
-| File         | Method | Routing         | Middlewares  | FilePath           |
-| ------------ | ------ | --------------- | ------------ | ------------------ |
-| src/game.ts  |        |                 |              |                    |
+| File         | Method | Routing         | Middlewares  | FilePath            |
+| ------------ | ------ | --------------- | ------------ | ------------------- |
+| src/game.ts  |        |                 |              |                     |
 |              | get    | /getGameById    | requireRead  | src/game.ts#L11-L11 |
 |              | get    | /getGameList    | requireRead  | src/game.ts#L12-L12 |
 |              | post   | /updateGameById | requireWrite | src/game.ts#L13-L13 |
 |              | delete | /deleteGameById | requireWrite | src/game.ts#L14-L14 |
-| src/index.ts |        |                 |              |                    |
+| src/index.ts |        |                 |              |                     |
 |              | use    | /user           | user         | src/index.ts#L8-L8  |
 |              | use    | /game           | game         | src/index.ts#L9-L9  |
-| src/user.ts  |        |                 |              |                    |
+| src/user.ts  |        |                 |              |                     |
 |              | get    | /getUserById    | requireRead  | src/user.ts#L10-L10 |
 |              | get    | /getUserList    | requireRead  | src/user.ts#L11-L11 |
 |              | post   | /updateUserById | requireWrite | src/user.ts#L12-L12 |
